b553aba8c6e8092c6d95854345710efc88a2174c
John Dowling <john.patrick.dowling@gmail.com>
2020-02-12 21:24:23 -0800

    Add set_led approute

--- api/webapi.py
+++ api/webapi.py
@@ -4,6 +4,7 @@ from protocol import get_temp
 from protocol import get_progress
 from protocol import get_status
 from protocol import set_temperature
+from protocol import set_led
 
 from flask import Flask
 from flask import jsonify
@@ -40,6 +41,11 @@ def temp(ip_address):
 @app.route("/<string:ip_address>/set-temp/<string:temp>")
 def set_temp(ip_address, temp):
     printer_info = set_temperature({'ip': ip_address, 'port': PORT}, temp)
+    return jsonify(printer_info)
+
+@app.route("/<string:ip_address>/set-led/<string:red>/<string:green>/<string:blue>")
+def set_led(ip_address, temp):
+    printer_info = set_led({'ip': ip_address, 'port': PORT}, red, green, blue)
     return jsonify(printer_info)
 
 @app.route("/<string:ip_address>/progress")
